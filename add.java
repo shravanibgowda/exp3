316ae6992f02c237f3db30d5fa43f2f84a27a94a
shravanibgowda <shravanibyregowda@gmail.com>
2026-03-03 21:26:49 +0530

    Add EvenOddCheck class to determine even or odd

---
+++ add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // Import the Scanner class to read user input
+
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer number:");
+        num = scanner.nextInt(); // Read the user input
+        scanner.close(); // Close the scanner
+
+        // Check if the number is divisible by 2 using the modulus operator
+        if (num % 2 == 0) {
+            System.out.println("The entered number " + num + " is Even");
+        } else {
+            System.out.println("The entered number " + num + " is Odd");
+        }
+    }
+}
